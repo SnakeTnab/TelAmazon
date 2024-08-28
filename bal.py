15fc9f6296dabded9d3fc81a62a028d4db1e70de
SnakeTnab <remy.simon.tnab@gmail.com>
2024-08-28 17:14:07 +0200

    Update bal.py

--- bal.py
+++ bal.py
@@ -2,21 +2,17 @@ import requests
 import json
 import streamlit as st
 from datetime import datetime
-import pandas as pd
 
 
-# Fetch data from the provided route URL
 def fetch_route_data(route_url, headers):
-    try:
-        response = requests.get(route_url, headers=headers)
-        response.raise_for_status()  # Raise an error for bad responses
+    response = requests.get(route_url, headers=headers)
+    if response.status_code == 200:
         return response.json()
-    except requests.exceptions.RequestException as e:
-        st.error(f"Failed to retrieve data for {route_url}: {e}")
+    else:
+        st.error(f"Failed to retrieve data for {route_url}: {response.status_code}")
         return None
 
 
-# Calculate the delivery rate to mail slots
 def calculate_mailbox_delivery_rate(route_data):
     total_delivered = 0
     delivered_to_mail_slot = 0
@@ -34,7 +30,6 @@ def calculate_mailbox_delivery_rate(route_data):
     return delivered_to_mail_slot / total_delivered
 
 
-# Process each route, fetch data, and calculate delivery rates
 def process_routes(prefixed_route_ids, headers):
     rates = []
     for route_url in prefixed_route_ids:
@@ -44,13 +39,12 @@ def process_routes(prefixed_route_ids, headers):
             route_id = route_url.split('/')[-1]  # extract route_id from route_url
             route_prefix = route_id.split('-')[0]  # extract prefix from route_id
             route_id_without_prefix = route_id.replace(f"{route_prefix}-", "")  # remove prefix from route_id
-            rate_str = f"{rate:.2%}".replace('.', ',')  # Convert the rate to a string
+            # Convert the rate to a string and replace the point with a comma
+            rate_str = f"{rate:.2%}".replace('.', ',')
             rates.append([route_id_without_prefix, rate_str])
     return rates
 
 
-# Fetch Amazon data based on the selected date
-@st.cache_data
 def search_amazon_data(local_date):
     cookies = {
     'lc-acbfr': 'fr_FR',
@@ -67,7 +61,7 @@ def search_amazon_data(local_date):
     'session-id': '258-3085241-4888146',
     'session-token': 'XCshqmMy2tEksfNzi0AOmv+If8ug67KKfsLdr4vu1bE5PPX8TR8meQujgZ5efPEEMegesYZsQFr8DXOITAd2shjCa/gSNVDE5/ZqwaTlfGXhmvtbr0Txfh36FzmMIedK42wAOXdZ/NwgMgbjVvdLZjU73diLYTOnXGeMkgzKetQl1Pc501bwxsUZIuUeIG5pIgyEbM9C4+xIc9vhyJYYq/3cdfLUtq3F4cHjPX3pLtuvtOCWoF5eY8ItHdTAGEYFMO6JzyXDuCCnBJ3vGrV8ajw5ZbBno48WiyqTl5gpvE6uOWkyro5YlKFxUrYsPYAhoxhCUloxqJ4B5fau7j7bKc/waYRcK3ehdvErBZcS0VQ',
     'cwr_s': 'eyJzZXNzaW9uSWQiOiIyYmQxMGU2MS1jMzY2LTQzMzUtYWIzOC00NGI4MTE0MWFjZjEiLCJyZWNvcmQiOnRydWUsImV2ZW50Q291bnQiOjI0OTMsInBhZ2UiOnsicGFnZUlkIjoiL29wZXJhdGlvbnMvZXhlY3V0aW9uL2l0aW5lcmFyaWVzIiwicGFyZW50UGFnZUlkIjoiL29wZXJhdGlvbnMvZXhlY3V0aW9uL3JvdXRlcyIsImludGVyYWN0aW9uIjoxMzEsInJlZmVycmVyIjoiaHR0cHM6Ly9sb2dpc3RpY3MuYW1hem9uLmZyL2FjY291bnQtbWFuYWdlbWVudC9kZWxpdmVyeS1hc3NvY2lhdGVzP3Byb3ZpZGVyVHlwZT1EQSZwcm92aWRlclN0YXR1cz1PTkJPQVJESU5HJnNlYXJjaFN0YXJ0PTAmc2VhcmNoU2l6ZT0xMDAiLCJyZWZlcnJlckRvbWFpbiI6ImxvZ2lzdGljcy5hbWF6b24uZnIiLCJzdGFydCI6MTcyMjk0ODQyODAyNn19',
-    }
+}
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
@@ -83,40 +77,40 @@ def search_amazon_data(local_date):
         'serviceAreaId': 'd459788b-6a73-48f9-a713-81afa0c59b66',
     }
 
-    try:
-        with st.spinner('Fetching data...'):
-            response = requests.get('https://logistics.amazon.fr/operations/execution/api/summaries',
-                                    params=params, headers=headers)
-            response.raise_for_status()
+    response = requests.get('https://logistics.amazon.fr/operations/execution/api/summaries', params=params,
+                            headers=headers)
 
-        data = response.json()
-        all_route_ids = [route['routeId'] for itinerary in data['itinerarySummaries']
-                         for route in itinerary['routes']]
-        prefixed_route_ids = [f"https://logistics.amazon.fr/operations/execution/api/routes/{route_id}"
-                              for route_id in all_route_ids]
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        all_route_ids = []
+
+        for itinerary in data['itinerarySummaries']:
+            route_ids = [route['routeId'] for route in itinerary['routes']]
+            all_route_ids.extend(route_ids)
+
+        prefixed_route_ids = [f"https://logistics.amazon.fr/operations/execution/api/routes/{route_id}" for route_id in
+                              all_route_ids]
 
         # Process each route and calculate mailbox delivery rates
         delivery_rates = process_routes(prefixed_route_ids, headers)
 
         # Display results using Streamlit
-        st.header("Mailbox Delivery Rates")
+        st.header("les taux de livraison en boites au lettres")
         st.subheader(f"Date: {local_date}")
         st.write("")
 
         # Create a DataFrame to display the results
+        import pandas as pd
         df = pd.DataFrame(delivery_rates, columns=["Route", "Rates"])
 
-        # Add conditional formatting
-        # st.dataframe(df.style.applymap(lambda x: 'background-color: green' if ',' in x and float(x.replace(',', '.').strip('%')) > 50 else 'background-color: red'), width=800, height=600)
+        st.dataframe(df, width=800, height=600)
 
-    except requests.exceptions.RequestException as e:
-        st.error(f"Failed to retrieve data: {e}")
+    else:
+        st.error(f"Failed to retrieve data: {response.status_code}")
 
 
 # Streamlit UI
 st.title("TNAB MAILSLOT")
 local_date = st.date_input("Date :", min_value=datetime(2022, 1, 1), max_value=datetime(2025, 1, 1))
-
-# Analyze button to trigger data fetch and processing
-if st.button("Analyze"):
+if st.button("Analyser"):
     search_amazon_data(local_date)
